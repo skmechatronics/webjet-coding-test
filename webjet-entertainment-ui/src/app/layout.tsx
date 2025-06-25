7a905d4ef595cffac660c6a6efcda306107ee490
Srikanth K. <sk.mechatronics@outlook.com>
2025-06-25 23:52:51 +1000

    Separation of Appheader

--- webjet-entertainment-ui/src/app/layout.tsx
+++ webjet-entertainment-ui/src/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import './globals.css';
 import { ReactNode } from 'react';
+import AppHeader from './components/app-header'
 
 export const metadata = {
   title: 'WebJet Entertainment System',
@@ -11,14 +12,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="h-full">
       <body className="flex flex-col min-h-screen h-full bg-gray-100 text-gray-900">
-        <header className="border-b bg-red-700 text-center text-gray-100">
-          <div className="max-w-6xl mx-auto px-4 pb-4 pt-2 flex justify-center">
-            <h1 className="text-3xl font-extrabold tracking-wide text-gray-100 drop-shadow-md underline">
-              WebJet Entertainment System
-            </h1>
-          </div>
-        </header>
-
+        <AppHeader/>
         <main className="flex-grow p-5 max-w-7xl mx-auto">
           {children}
         </main>
